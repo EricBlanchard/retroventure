00917d291d313eecf512ff3363ab520f487e2b3a
Eric Blanchard <eric.x2@gmail.com>
2018-09-29 12:12:56 -0400

    Web server is up and running, tutorial done with modals being loaded

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+//webpack.config.js
+var path = require('path');
+var webpack = require('webpack');
+module.exports = {
+ entry: './client/index.js',
+ output: {
+  path: path.join(__dirname, 'client'),
+  filename: 'bundle.js'
+ },
+ node: {
+    console: false,
+    fs: 'empty',
+    net: 'empty',
+    tls: 'empty'
+  },
+ module: {
+  loaders: [{
+   test: /.jsx?$/,
+   loader: 'babel-loader',
+   exclude: /node_modules/,
+   query: {
+    presets: ['es2015', 'react']
+   }
+  },
+  {
+   test: /\.css$/,
+   loader: "style-loader!css-loader"
+  }]
+ }
+}
